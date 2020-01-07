b8b73a64ae04783b26de0ff61224223062938719
the-beta-core <shoutatrikjanssen@gmail.com>
2020-01-07 12:18:58 +0100

    1

--- template/wp-admin-form.php
+++ template/wp-admin-form.php
@@ -224,33 +224,38 @@
 /* THE FOOTER.              */
 
 $bcALG_my_plugins = array(
-                array(
-                    'slug'=>'rebranded-pro-the-agency-toolkit',
-                    'name'=>'Re:Branded Pro | The Agency Toolkit',
-                    'features'=>'https://betacore.tech/plugins/rebranded-pro-agency-toolkit/',
-                    'content'=>'This is a total rebranding package for the WordPress admin built for for agencies, designers and website builders. This plugin also protects essential parts of the WordPress installation in order to create an awesome user experience for the client on the WP-admin dashboard. My other plugins (Super Simple Age Gate, Super Simple Site Offline, Simple Analytics Tag) hook right in! So it feels as if they are part of Re:Branded. Or are they?' ),
-                array(
-                    'slug'=>'super-simple-site-offline-beta',
-                    'name'=>'Super Simple Site Offline',
-                    'features'=>'https://betacore.tech/plugins/super-simple-site-offline-for-wordpress/',
-                    'content'=>'Site offline plugins are made awesome again with this piece of code. While most site offline plugins are bulky, intrusive and annoying this one is as light as a feather and has no paid options. The nav item is neatly tucked away within the settings menu so it feels like it is part of WordPress.' ),
-                array(
-                    'slug'=>'simple-analytics-tag-beta',
-                    'name'=>'Simple Analytics Tag',
-                    'features'=>'https://betacore.tech/plugins/simple-analytics-tag-for-wordpress/',
-                    'content'=>'Simple Analytics Tag helps you get up and running quick. This plugin has a non-intrusive interface and fits very well within the Wordpress Settings menu. Just paste in the ID from Google Tagmanager or Google Analytics and you are good to go.' ),
-                array(
-                    'slug'=>'super-simple-age-gate-beta',
-                    'name'=>'Super Simple Age Gate',
-                    'features'=>'https://betacore.tech/plugins/super-simple-age-gate-for-wordpress/',
-                    'content'=>"Do you have to filter out younger visitors? With this super simple age gate you'll fix those age restrictions quickly. Ment for webshops and other types of websites that has to have a curtain where people below your set age can't peek behind.." ),
-                array(
-                    'slug'=>'age-checkbox-for-woocommerce',
-                    'name'=>'Age Checkbox for Woocommerce',
-                    'features'=>'https://betacore.tech/plugins/age-checkbox-for-woocommerce/',
-                    'content'=>"Complementary to the Super Simple Age Gate this adds an additional checkbox to the checkout form so customers have to confirm that they are age 18 (you can modify the age) or up." )
-            
-            );
+    array(
+        'slug'=>'rebranded-pro-the-agency-toolkit',
+        'name'=>'Re:Branded Pro | The Agency Toolkit',
+        'features'=>'https://betacore.tech/plugins/rebranded-pro-agency-toolkit/',
+        'content'=>'This is a total rebranding package for the WordPress admin built for for agencies, designers and website builders. This plugin also protects essential parts of the WordPress installation in order to create an awesome user experience for the client on the WP-admin dashboard. My other plugins (Super Simple Age Gate, Super Simple Site Offline, Simple Analytics Tag) hook right in! So it feels as if they are part of Re:Branded. Or are they?' ),
+    array(
+        'slug'=>'super-simple-site-offline-beta',
+        'name'=>'Super Simple Site Offline',
+        'features'=>'https://betacore.tech/plugins/super-simple-site-offline-for-wordpress/',
+        'content'=>'Site offline plugins are made awesome again with this piece of code. While most site offline plugins are bulky, intrusive and annoying this one is as light as a feather and has no paid options. The nav item is neatly tucked away within the settings menu so it feels like it is part of WordPress.' ),
+    array(
+        'slug'=>'super-simple-recaptcha-v3',
+        'name'=>'Super Simple Recapthca V3',
+        'features'=>'https://betacore.tech/plugins/super-simple-recaptcha-v3/',
+        'content'=>'Got spammers? Add Recaptcha V3 in an instant! Works for any contact form. Uses Google Recaptcha V3. A how-to is included.' ),
+    array(
+        'slug'=>'simple-analytics-tag-beta',
+        'name'=>'Simple Analytics Tag',
+        'features'=>'https://betacore.tech/plugins/simple-analytics-tag-for-wordpress/',
+        'content'=>'Simple Analytics Tag helps you get up and running quick. This plugin has a non-intrusive interface and fits very well within the Wordpress Settings menu. Just paste in the ID from Google Tagmanager or Google Analytics and you are good to go.' ),
+    array(
+        'slug'=>'super-simple-age-gate-beta',
+        'name'=>'Super Simple Age Gate',
+        'features'=>'https://betacore.tech/plugins/super-simple-age-gate-for-wordpress/',
+        'content'=>"Do you have to filter out younger visitors? With this super simple age gate you'll fix those age restrictions quickly. Ment for webshops and other types of websites that has to have a curtain where people below your set age can't peek behind.." ),
+    array(
+        'slug'=>'age-checkbox-for-woocommerce',
+        'name'=>'Age Checkbox for Woocommerce',
+        'features'=>'https://betacore.tech/plugins/age-checkbox-for-woocommerce/',
+        'content'=>"Complementary to the Super Simple Age Gate this adds an additional checkbox to the checkout form so customers have to confirm that they are age 18 (you can modify the age) or up." )
+
+);
 
 // get the slug of this plugin
 $get_slug = explode('/', plugin_basename( __FILE__ ));
