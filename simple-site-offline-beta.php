8c1e2582915abf227b6e2a7b2f0c6a6d10e2495d
the-beta-core <shoutatrikjanssen@gmail.com>
2020-08-15 00:11:36 +0200

    1

--- simple-site-offline-beta.php
+++ simple-site-offline-beta.php
@@ -3,7 +3,7 @@
 * Plugin Name: Super Simple Site Offline
 * Plugin URI: https://betacore.tech/plugins/super-simple-site-offline-for-wordpress/
 * Description:  Hide or redirect your website in an instant! The Super Simple Site Offline Plugin does exactly that and is above all easy to customize and track via Google Analytics or Google Tagmanager. But for your visitor just a nice little maintenance message or redirect. Nothing more.
-* Version: 1.7.7
+* Version: 1.8
 * Author: Rik Janssen (Beta)
 * Author URI: https://betacore.tech/
 * Text Domain: betaoffline
